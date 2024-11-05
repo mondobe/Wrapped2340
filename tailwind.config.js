bd85b252e67cff976db9378bfd90530f49c6cbcd
thatdudegrantt <polazzo.grant@gmail.com>
2024-11-04 23:36:13 -0500

    Login Register Front End
    - Implemented Daisy and TailWind for completed style of the page, added more themes, added eye icons for front end understanding of password security, fixed various reallignment bugs and am committing this to pull from main to update the home page. utilized SVG icons deisgned for tailwind for the hidden visible symbols for the password fields, will link this login register page to the home page when i add the home front end and added a modal for registered user to tab over to log in page via JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,52 @@
 
 module.exports = {
   content: [
-      './users/templates/users/*.html',
-    './templates/**/*.html',
-    './**/templates/**/*.html',
-    './static/js/**/*.js',
+    './Wrapped2340/home/templates/**/*.html',  // Home app templates
+    './Wrapped2340/users/templates/**/*.html', // Users app templates
+    './Wrapped2340/slides/templates/**/*.html', // Slides app templates
+    './Wrapped2340/**/templates/**/*.html',     // Catch-all for any other
+
+   // app templates
+    './static/**/*.css',                        // All static CSS files
+    './static/js/**/*.js',                      // All static JS files
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+
+    base: true,
   },
   theme: {
     extend: {},
