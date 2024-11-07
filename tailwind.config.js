4355db95e3183d7646a91fa7ff12990ac7791f7e
kdcao1 <kdcao1@yahoo.com>
2024-11-07 01:59:42 -0500

    debugged tailwind/daisy
    - Daisy should be fully implemented now
    - Migrated navbar to daisy
    - Added theme swapper into settings page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,56 +5,25 @@ module.exports = {
     './Wrapped2340/home/templates/**/*.html',  // Home app templates
     './Wrapped2340/users/templates/**/*.html', // Users app templates
     './Wrapped2340/slides/templates/**/*.html', // Slides app templates
+    './Wrapped2340/common/templates/**/*.html', // Common app templates
     './Wrapped2340/**/templates/**/*.html',     // Catch-all for any other
 
    // app templates
     './static/**/*.css',                        // All static CSS files
     './static/js/**/*.js',                      // All static JS files
   ],
-  daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-      "dim",
-      "nord",
-      "sunset",
-    ],
-
-    base: true,
-  },
   theme: {
     extend: {},
   },
   plugins: [
-      require('daisyui'),
+    require("@tailwindcss/typography"),
+    require('daisyui'),
   ],
+  daisyui: {
+    themes: true,
+    styled: true,
+    base: true,
+  },
 }
 
 
