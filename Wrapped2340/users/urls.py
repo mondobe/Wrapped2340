14cfaee9e1c138c61b936c162ebabdae6c042016
kdcao1 <kdcao1@yahoo.com>
2024-11-10 04:08:30 -0500

    bug fix
    - removed outdated url
    - fixed tab memory

--- Wrapped2340/users/urls.py
+++ Wrapped2340/users/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path("reset-password-complete/", views.WrappedPasswordResetCompleteView.as_view(), name="password-reset-complete"),
     path('link-spotify/', views.LinkSpotify.as_view(), name='link-spotify'),
     path('rotate-invite-token/', views.RotateInviteTokenView.as_view(), name='rotate-invite-token'),
-    path("login_register/", views.login_register, name="login_register"),
     path('dev-feedback/', views.dev_feedback, name='dev-feedback'),
     path('manage-feedback/', views.manage_feedback, name='manage-feedback'),
 ]
