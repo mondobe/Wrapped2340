c37a6cb3da0799b4340c6c959d3049e40dba18f7
thatdudegrantt <polazzo.grant@gmail.com>
2024-11-13 16:28:20 -0500

    v2
    removed default in favor of the summary as default
    general bug fixes

--- Wrapped2340/slides/views.py
+++ Wrapped2340/slides/views.py
@@ -13,20 +13,19 @@ class SlidesView(LoginRequiredMixin, TemplateView):
         # Map page IDs to slide titles or any other data specific to each slide
         slide_titles = {
             1: 'Get Ready to Travel the World with Audioscape 🌎',      # Transition slide
-            2: 'Regions Yoyr Favorite Songs are From',                # Info slide
+            2: 'Regions Your Favorite Songs are From',                # Info slide
             3: 'Genre-ous Moments Ahead 🎶',            # Transition slide
             4: 'Top Genres',                            # Info slide
             5: 'Counting Down the Beats 🔟',            # Transition slide
             6: 'Top 10 Songs',                          # Info slide
             7: 'Artist Appreciation Time 🎤',           # Transition slide
             8: 'Top Artists',                           # Info slide
-            9: 'Pack Your Vibes with Audioscape.AI 🌴',                    # Transition slide
+            9: 'Pack Your Vibes with Audioscape.AI 🌴',  # Transition slide
             10: 'Your AI Travel Suggestion',            # Info slide
             11: 'Buzzing With Excitement 🐝',           # Transition slide
             12: 'What’s the Buzz? (Game 1)',            # Info slide
-            13: '2nd Game intro',                          # Transition slide
-            14: 'Game 2',                          # Info slide
-            15: 'Summary',                              # Summary slide (shareable/saveable)
+            13: '2nd Game intro',                       # Transition slide
+            14: 'Game 2',                               # Info slide
         }
 
         # Set the page title or other context variables based on the page ID
