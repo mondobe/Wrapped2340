014ba32c88d848acc6b36ca243a4a7377492c952
kdcao1 <kdcao1@yahoo.com>
2024-12-01 23:25:50 -0500

    Update settings.py

--- Wrapped2340/settings.py
+++ Wrapped2340/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['wrapped.lovelylilo.com']
 
 LOGIN_URL = reverse_lazy("users:login")
 
